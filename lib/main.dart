e4ee63360464a176c4688bd25c2fa084ed345aab
agungribowo <ribowoagung@gmail.com>
2023-06-07 20:13:16 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage()
+  ));
+}
+
+class HomePage extends StatelessWidget {
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        leading: Icon(Icons.home),
+        title: Center(
+          child: Text("Niomic Apps")
+        ),
+        actions: <Widget>[
+          Icon(Icons.search),
+        ],
+       ),
+    body: FlatButton(
+        onPressed: (){
+          print("Ini adalah Button Niomic");
+        },
+        child: Text("Ini adalah Button Niomic")
+        ),
+    );
+    
+    }
+  }
